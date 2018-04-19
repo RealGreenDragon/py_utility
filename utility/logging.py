6ea25382223f2b19ede988efa9c2eaf8f1a52a3e
MagicGreenDragon <14246920+MagicGreenDragon@users.noreply.github.com>
2018-04-19 17:04:16 +0200

    Create logging.py

---
+++ utility/logging.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2018  Daniele Giudice
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import logging
+
+# NullHandler for Python previous v2.7
+if hasattr(logging, 'NullHandler'):
+    NullHandler = logging.NullHandler
+else:
+    class NullHandler(logging.Handler):
+        def handle(self, record):
+            pass
+
+        def emit(self, record):
+            pass
+
+        def createLock(self):
+            self.lock = None
